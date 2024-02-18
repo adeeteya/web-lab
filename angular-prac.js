173f9dd4d8e82896aa5358f61ef6c84da4074acf
Aditya <62930521+adityar224@users.noreply.github.com>
2024-02-18 02:09:00 -0500

    📝 updated username

--- angular-prac.js
+++ angular-prac.js
@@ -1,15 +1,17 @@
-let myApp = angular.module("myApp", []);
-myApp.controller("myController", function ($scope) {
-    $scope.namesList = [
-        {name:"Arman Cotton"},
-        {name:"Shanay Johns"},
-        {name:"Jayden Wheatley"},
-        {name:"Amber Tucker"},
-        {name:"Alia Beaumont"},
-        {name:"Natan Daniels"},
-        {name:"Chanice Barry"},
-        {name:"Lucille Hanson"},
-        {name:"Rhys Redmond"},
-        {name:"Shelley Bolton"}
-    ]
+let myApp = angular.module("myApp", ["ngRoute"]);
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", { template: "<h1>Home</h1>" })
+        .when("/red", { template: "<h1>Red</h1>" })
+        .otherwise({ template: "<h1>Invalid Url</h1>" });
+});
+myApp.component("info", {template:"Hello {{ $ctrl.name }}",controller:function(){
+    this.name="Aditya";
+}});
+myApp.directive('headingHere', function () {
+    return {
+        template: "<h1>Welcome to this app</h1>"
+    };
+});
+myApp.controller("myCtrl", function ($scope) {
 });
